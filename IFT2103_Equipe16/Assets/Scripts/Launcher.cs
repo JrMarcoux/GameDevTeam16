b7abeb6988e72799b452bb4301482d63247beba5
Gigaw0lf <gabriel-giguere@hotmail.com>
2018-09-24 01:08:45 -0400

    power bar
    il reste à la linker à la vitesse/position

--- IFT2103_Equipe16/Assets/Scripts/Launcher.cs
+++ IFT2103_Equipe16/Assets/Scripts/Launcher.cs
@@ -8,15 +8,17 @@ public class Launcher : MonoBehaviour {
 	public bool isDead = false;
 	public string enemyTag;
 
-
-	public IEnumerator launch()
+    public IEnumerator launch()
 	{
-		//attendre que le joueur appuie sur Espace pour enclancher le lancer
+        powerBarScript powerBar = GameObject.Find("powerBar").GetComponent<powerBarScript>();
+        //attendre que le joueur appuie sur Espace pour enclancher le lancer
+        powerBar.ToggleOn();
 		while (!Input.GetKeyDown(KeyCode.Space))
-		{
+		{     
 			yield return null;
 		}
-		GameObject currentProj = Instantiate(projectile, shotPoint.position, shotPoint.rotation); //instancier le projectile
+        powerBar.ToggleOff();
+        GameObject currentProj = Instantiate(projectile, shotPoint.position, shotPoint.rotation); //instancier le projectile
 		currentProj.GetComponent<RangeAttack>().nameOfTargets = enemyTag; //assigner le nom de la cible
 
 	}
---
+++ IFT2103_Equipe16/Assets/Scripts/powerBarScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class powerBarScript : MonoBehaviour {
+
+    public float fullWidth = 256f;
+    public bool powerBarMoving = false;
+
+    public Image powerBar;
+    public float timeFill = 5f;
+    private float direction=1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (powerBarMoving == true)
+        {
+            if (powerBar.fillAmount <= 0)
+            {
+                direction = 1;
+            }
+            else if (powerBar.fillAmount >= 1)
+            {
+                direction = -1;
+            }
+            powerBar.fillAmount += direction / (fullWidth * timeFill * Time.deltaTime);
+
+        }
+	}
+
+    public void ToggleOn()
+    {
+        powerBarMoving = true;
+    }
+
+    public void ToggleOff()
+    {
+        powerBarMoving = false;
+    }
+
+    public float GetAmount()
+    {
+        return powerBar.fillAmount;
+    }
+}

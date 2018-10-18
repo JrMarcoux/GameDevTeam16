f55551ab9c92b60ae97c40ec717bf10f175800af
Alain Junior Marcoux <alain-junior.marcoux.1@ulaval.ca>
2018-10-18 17:27:40 -0400

    ajout de la classe pour la physique du lancer

---
+++ IFT2103_Equipe16/Assets/Scripts/BallisticPhysics.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallisticPhysics : MonoBehaviour {
+
+	
+	public float verticalMaxDisplacement = 2f;
+	[Range(-0.01f, -100)]
+	public float gravity = -18f;
+
+	//lancer le projectile avec la physique
+	public void BallisticLaunch(Rigidbody projectile, GameObject target, float offsetLaunch = 0.50f)
+	{
+		Physics.gravity = Vector3.up * gravity;
+
+		//calculer le déplacement
+		float displacementY = target.transform.position.y - projectile.position.y;
+		Vector3 displacementXZ;
+		if (0.40 < offsetLaunch && offsetLaunch < 0.60){
+			displacementXZ = new Vector3(target.transform.position.x - projectile.position.x, 0, target.transform.position.z - projectile.position.z);
+		}
+		else{
+			displacementXZ = new Vector3(target.transform.position.x * 2 * offsetLaunch - projectile.position.x, 0, target.transform.position.z * 2 * offsetLaunch - projectile.position.z);
+		}
+
+		//ajuster la hauteur max si la cible est plus haute que la hauteur max, 
+		if (target.transform.position.y > verticalMaxDisplacement){
+			verticalMaxDisplacement = target.transform.position.y + target.GetComponent<Collider>().bounds.size.y;
+		}
+		//calcuer la vélocité
+		Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * verticalMaxDisplacement);
+		float time = Mathf.Sqrt(-2 * verticalMaxDisplacement / gravity) + Mathf.Sqrt(2 * (displacementY - verticalMaxDisplacement) / gravity);
+		Vector3 velocityXY = displacementXZ / time;
+
+		//lancer le projectile
+		projectile.velocity = velocityXY + velocityY;
+	}
+}
--- IFT2103_Equipe16/Assets/Scripts/RangeAttack.cs
+++ IFT2103_Equipe16/Assets/Scripts/RangeAttack.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class RangeAttack : MonoBehaviour {
 	private Rigidbody projectile;
+	private BallisticPhysics physicScript;
 	public string nameOfTargets;
 	public float lifeTime = 5f;
 	public List<GameObject> targets;
 	private GameObject target;
-	public float verticalMaxDisplacement = 2f;
-	[Range(-0.01f,-100)]
-	public float gravity = -18f;
 	private GameObject gameManager;
   public powerBarScript powerBar;
 	public GameObject outBoundaries;
 	private bool checkAABB = false;
 	private bool outsideAABB = false;
-    private bool targetAABB = false;
+   private bool targetAABB = false;
 
     void Start()
 	{
+		physicScript = gameObject.GetComponent<BallisticPhysics>();
 		outBoundaries = GameObject.Find("AABB");
 		checkAABB = false;
 		powerBar = GameObject.Find("powerBar").GetComponent<powerBarScript>();
@@ -46,9 +45,9 @@ public class RangeAttack : MonoBehaviour {
 			outsideAABB = !outBoundaries.GetComponent<CollisionAABB>().CheckIfObjectIsInAABB(gameObject);
             targetAABB = target.GetComponent<CollisionAABB>().CheckIfObjectIsInAABB(gameObject);
 		}
-		if (outsideAABB)
+		if (outsideAABB)//si la balle dépasse les limites de la boite AABB, elle est détruite
 		{
-			DestroyProjectile();//si la balle dépasse les limites de la boite AABB, elle est détruite
+			DestroyProjectile();
 		}
         if (targetAABB)
         {
@@ -60,37 +59,10 @@ public class RangeAttack : MonoBehaviour {
 	void Launch()
 	{
 		checkAABB = true; // on enclanche la détection pour regarder si la balle dépasse la boite AABB
-		Physics.gravity = Vector3.up * gravity;
 		projectile.useGravity = true;
-		projectile.velocity = CalcLaunchVelocity();
+		float offset = powerBar.GetAmount(); //valeur de la powerbar
+		physicScript.BallisticLaunch(projectile, target, offset); //lancer le projectile avec la physique
 		Invoke("DestroyProjectile", 5.0f);
-	}
-	//calculer la vitesse du lancer
-	Vector3 CalcLaunchVelocity()
-	{       
-
-    float displacementY = target.transform.position.y - projectile.position.y;
-
-    Vector3 displacementXZ;
-		//valeur de la powerbar
-    if (0.40 < powerBar.GetAmount() && powerBar.GetAmount() < 0.60)
-    {
-        displacementXZ = new Vector3(target.transform.position.x - projectile.position.x, 0, target.transform.position.z - projectile.position.z);
-    }
-    else
-    {
-        displacementXZ = new Vector3(target.transform.position.x * 2 * powerBar.GetAmount() - projectile.position.x, 0, target.transform.position.z * 2 * powerBar.GetAmount() - projectile.position.z);
-    }
-		
-		//si la cible est plus haute que la hauteur max, ajuster la hauteur max
-		if(target.transform.position.y > verticalMaxDisplacement)
-		{
-			verticalMaxDisplacement = target.transform.position.y + target.GetComponent<Collider>().bounds.size.y;
-		}         
-		Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * verticalMaxDisplacement);
-		float time = Mathf.Sqrt(-2 * verticalMaxDisplacement / gravity) + Mathf.Sqrt(2 * (displacementY - verticalMaxDisplacement) / gravity);
-		Vector3 velocityXY = displacementXZ / time;
-		return velocityXY + velocityY ; 
 	}
 	//détruitre le projectile
 	void DestroyProjectile()

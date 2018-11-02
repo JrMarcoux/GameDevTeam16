cce8fb654efc37ebbf385be1cb3c6a28d3dcfe09
Danick Proulx <tidan80@gmail.com>
2018-11-02 18:47:54 -0400

    ajout d'un HUD permetant de voir qui est en vie

---
+++ IFT2103_Equipe16/Assets/Scripts/enemiesLivesScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemiesLivesScript : MonoBehaviour
+{
+
+    private List<Image> lives;
+    private int index;
+    private GameObject gameManager;
+    private int nbrLives;
+
+    // Use this for initialization
+    void Start()
+    {
+        lives = new List<Image>();
+        foreach (Transform child in transform)
+        {
+            lives.Add(child.GetComponent<Image>());
+        }
+        index = 0;
+        gameManager = GameObject.FindGameObjectWithTag("Game manager");
+        nbrLives = gameManager.GetComponent<GameManager>().enemiesAlive.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.GetComponent<GameManager>().enemiesAlive.Count < nbrLives)
+        {
+            lives[index++].enabled = true;
+            nbrLives--;
+        }
+    }
+}

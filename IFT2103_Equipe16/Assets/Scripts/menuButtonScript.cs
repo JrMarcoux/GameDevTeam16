fcfab201bcf99943142bb551a19464eaf9d01bd0
Gigaw0lf <gabriel-giguere@hotmail.com>
2018-12-05 16:57:47 -0500

    pointeur menu + document

---
+++ IFT2103_Equipe16/Assets/Scripts/menuButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuButtonScript : MonoBehaviour {
+
+    public GameObject pointer;
+
+    public void changePointerPosition()
+    {
+        pointer.transform.position = new Vector3(pointer.transform.position.x, transform.position.y, pointer.transform.position.z);
+    }
+}

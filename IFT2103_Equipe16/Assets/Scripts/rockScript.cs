3a050b4e25f78c3e501b7a7139d9fe998b8a59f4
Gigaw0lf <gabriel-giguere@hotmail.com>
2018-12-05 00:28:13 -0500

    noise and animation gen

---
+++ IFT2103_Equipe16/Assets/Scripts/rockScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rockScript : MonoBehaviour {
+
+    private Renderer m_renderer;
+    private texturesGenerator texturesGenerator;
+
+    void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+        m_renderer.material.EnableKeyword("_NORMALMAP");
+        texturesGenerator = GameObject.FindGameObjectWithTag("Game manager").GetComponent<texturesGenerator>();
+    }
+
+    void Update()
+    {
+        m_renderer.material.SetTexture("_BumpMap", texturesGenerator.textureRock);
+    }
+}
---
+++ IFT2103_Equipe16/Assets/Scripts/texturesGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class texturesGenerator : MonoBehaviour {
+
+    public int width = 256;
+    public int height = 256;
+    public float scale = 20f;
+    public float offsetX = 0f;
+    public float offsetY = 0f;
+
+    public Texture2D textureMagma;
+    public Texture2D textureRock;
+
+    public Color magicColor;
+    private Color baseColor;
+
+    private void Start()
+    {
+        baseColor = new Color(0.5f,0,0.5f);
+        textureRock = GeneratePerlinTexture(0, 0);
+    }
+
+    void Update () {
+        if(offsetX >= width)
+        {
+            offsetX = 0;
+        }
+        else
+        {
+            offsetX += 0.1f;
+        }
+        offsetY = Mathf.PingPong(Time.time, 4.0f);
+        textureMagma = GeneratePerlinTexture(offsetX, offsetY);  
+
+        float emission = Mathf.PingPong(Time.time, 1.0f);
+        magicColor = baseColor * Mathf.LinearToGammaSpace(emission);
+    }
+
+    Texture2D GeneratePerlinTexture(float offsetXPerl, float offsetYPerl)
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color color = CalculatePerlinColor(x, y,offsetXPerl,offsetYPerl);
+                texture.SetPixel(x, y, color);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculatePerlinColor(int x, int y, float offsetXPerl, float offsetYPerl)
+    {
+        float xUnit = (float)x / width * scale + offsetXPerl;
+        float yUnit = (float)y / height * scale + offsetYPerl;
+        float sample = Mathf.PerlinNoise(xUnit, yUnit);
+        return new Color(sample, sample, sample);
+    }
+}

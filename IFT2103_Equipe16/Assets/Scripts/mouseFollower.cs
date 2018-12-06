4b5b06c7a9567e4732fe11526f04981ec5afd97b
Gigaw0lf <gabriel-giguere@hotmail.com>
2018-12-06 18:16:25 -0500

    pointeur menu VFX

---
+++ IFT2103_Equipe16/Assets/Scripts/mouseFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseFollower : MonoBehaviour {
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update () {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10;
+
+        Vector3 mouseScreenToWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = mouseScreenToWorld;
+    }
+}

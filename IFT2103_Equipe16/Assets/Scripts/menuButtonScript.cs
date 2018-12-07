e4f0d84700d8aec64f1ba1fd21b9aa2db91f8e73
Danick Proulx <tidan80@gmail.com>
2018-12-06 21:42:14 -0500

    Ajout de musique et de SFX dans le menu

--- IFT2103_Equipe16/Assets/Scripts/menuButtonScript.cs
+++ IFT2103_Equipe16/Assets/Scripts/menuButtonScript.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class menuButtonScript : MonoBehaviour {
+public class menuButtonScript : MonoBehaviour
+{
 
     public GameObject pointer;
+    private GameObject camera;
+
+    void Awake()
+    {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
 
     public void changePointerPosition()
     {
         pointer.transform.position = new Vector3(pointer.transform.position.x, transform.position.y, pointer.transform.position.z);
+    }
+
+    public void playOnEnterSound()
+    {
+        camera.GetComponents<AudioSource>()[1].Play();
+    }
+
+    public void playOnClickSound()
+    {
+        camera.GetComponents<AudioSource>()[2].Play();
     }
 }

bedcd924c2d7c2248e5a1172daba3340379d293a
Alain Junior Marcoux <alain-junior.marcoux.1@ulaval.ca>
2018-12-16 12:59:41 -0500

    slice player in photoshop + new animation for each part

---
+++ IFT2103_Equipe16/Assets/Scripts/animPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animPlayer : MonoBehaviour {
+
+	public Animator body;
+	public Animator bodyPart;
+	bool isDead = false;
+	bool stopCheck = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!stopCheck)
+		{
+			isDead = GetComponent<Launcher>().isDead;
+			if (isDead)
+			{
+				stopCheck = true;
+				body.SetBool("isDead",true);
+				bodyPart.SetBool("isDead", true);
+			}
+
+		}
+	}
+}

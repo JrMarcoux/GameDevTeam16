53f4b2f07f40674f6975c26dec3b05a1bbf8cd08
Alain Junior Marcoux <alain-junior.marcoux.1@ulaval.ca>
2018-12-16 15:57:37 -0500

    interface pour personnaliser l'avatar + choix de couleur primaire,secondaire

---
+++ IFT2103_Equipe16/Assets/Scripts/CustomAvatarMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomAvatarMenu : MonoBehaviour {
+
+	public SpriteRenderer avatarBody;
+	public SpriteRenderer avatarBodyPart;
+	public Color blue;
+	public Color red;
+	public Color green;
+	public Color orange;
+	public Color purple;
+	public Color pink;
+	public Color grey;
+	public Color yellow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void changePrimaryBlue()
+	{
+		avatarBody.color = blue;
+	}
+	public void changePrimaryRed()
+	{
+		avatarBody.color = red;
+	}
+	public void changePrimaryGray()
+	{
+		avatarBody.color = grey;
+	}
+	public void changePrimaryOrange()
+	{
+		avatarBody.color = orange;
+	}
+
+	public void changeSecondaryPurple()
+	{
+		avatarBodyPart.color = purple;
+	}
+	public void changeSecondaryPink()
+	{
+		avatarBodyPart.color = pink;
+	}
+	public void changeSecondaryGreen()
+	{
+		avatarBodyPart.color = green;
+	}
+	public void changeSecondaryYellow()
+	{
+		avatarBodyPart.color = yellow;
+	}
+
+	public void saveSetting()
+	{
+		PlayerPrefs.SetString("PrimaryColor", ColorToHex(avatarBody.color) );
+		PlayerPrefs.SetString("SecondaryColor", ColorToHex(avatarBodyPart.color));
+	}
+
+	string ColorToHex(Color32 color)
+	{
+		string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+		return hex;
+	}
+}
--- IFT2103_Equipe16/Assets/Scripts/animPlayer.cs
+++ IFT2103_Equipe16/Assets/Scripts/animPlayer.cs
@@ -8,15 +8,29 @@ public class animPlayer : MonoBehaviour {
 	public Animator bodyPart;
 	bool isDead = false;
 	bool stopCheck = false;
+	public SpriteRenderer bodyRenderer;
+	public SpriteRenderer bodyPartRenderer;
+
 
 
 	// Use this for initialization
 	void Start () {
-		
+
+		bodyRenderer.color = HexToColor(PlayerPrefs.GetString("PrimaryColor"));
+		bodyPartRenderer.color = HexToColor(PlayerPrefs.GetString("SecondaryColor"));
+
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
+	Color HexToColor(string hex)
+	{
+		byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+		byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+		byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+		return new Color32(r, g, b, 255);
+	}
+
+		// Update is called once per frame
+		void Update () {
 		if (!stopCheck)
 		{
 			isDead = GetComponent<Launcher>().isDead;
